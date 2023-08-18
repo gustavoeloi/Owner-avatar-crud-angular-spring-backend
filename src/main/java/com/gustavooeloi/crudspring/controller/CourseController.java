0805167e2efadc6c1d699a081a1c04b8984319ce
gustavooeloi <gustavooeloi@gmail.com>
2023-08-18 16:55:58 -0300

    Adicionando arquivos do Spring

---
+++ src/main/java/com/gustavooeloi/crudspring/controller/CourseController.java
@@ -0,0 +1,17 @@
+package com.gustavooeloi.crudspring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+
+    @GetMapping
+    public List<Object> list(){
+        return null;
+    }
+}
